88c3890077d23417b5589abb3f5c954628e6728e
westonludeke <westonludeke@gmail.com>
2020-12-15 12:54:00 -0600

    Finished OO Basics 1

---
+++ 1_oo_basics_classes_and_objects/1.rb
@@ -0,0 +1,16 @@
+# Update the following code so that, instead of printing the values, each statement prints the name of the class to which it belongs.
+
+=begin
+
+Expected output:
+
+String
+Integer
+Array
+
+=end
+
+puts "Hello".class
+puts 5.class
+puts [1, 2, 3].class
+
