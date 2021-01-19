9a2a765c311dd5dfd05d95c6bdcb6f3261683336
westonludeke <westonludeke@gmail.com>
2021-01-18 18:50:53 -0600

    Added notes file to redo exercises

---
+++ medium_1/2.rb
@@ -0,0 +1,53 @@
+# A fixed-length array is an array that always has a fixed number of elements. Write a class that implements a fixed-length array, and provides the necessary methods to support the following code:
+
+fixed_array = FixedArray.new(5)
+puts fixed_array[3] == nil
+puts fixed_array.to_a == [nil] * 5
+
+fixed_array[3] = 'a'
+puts fixed_array[3] == 'a'
+puts fixed_array.to_a == [nil, nil, nil, 'a', nil]
+
+fixed_array[1] = 'b'
+puts fixed_array[1] == 'b'
+puts fixed_array.to_a == [nil, 'b', nil, 'a', nil]
+
+fixed_array[1] = 'c'
+puts fixed_array[1] == 'c'
+puts fixed_array.to_a == [nil, 'c', nil, 'a', nil]
+
+fixed_array[4] = 'd'
+puts fixed_array[4] == 'd'
+puts fixed_array.to_a == [nil, 'c', nil, 'a', 'd']
+puts fixed_array.to_s == '[nil, "c", nil, "a", "d"]'
+
+puts fixed_array[-1] == 'd'
+puts fixed_array[-4] == 'c'
+
+begin
+  fixed_array[6]
+  puts false
+rescue IndexError
+  puts true
+end
+
+begin
+  fixed_array[-7] = 3
+  puts false
+rescue IndexError
+  puts true
+end
+
+begin
+  fixed_array[7] = 3
+  puts false
+rescue IndexError
+  puts true
+end
+
+# The above code should output true 16 times.
+
+
+
+
+
---
+++ notes.md
@@ -0,0 +1,64 @@
+### Easy 1
+
+1. Medium
+2.
+3.
+4.
+5.
+6.
+7.
+8.
+9.
+10.
+
+### OO Basics - Accessor Methods
+
+1. Easy  
+2. Easy
+3. Easy - Redo
+4. Medium - Redo
+5. Medium - Redo
+6. Medium - Redo
+7. Easy - Redo
+8. Medium - redo - no idea
+9. Medium - redo - no idea
+10. Medium - completed but redo
+
+### OO Basics - Inheritance
+
+1. Easy
+2. Easy - Redo
+3. Easy - Redo - Super
+4. Medium - Redo - Super
+5. Easy
+6. Easy
+7. Easy
+8. Easy
+9. Easy
+10. Easy - Redo
+
+### OO Basics: Classes and Objects 2
+
+1. Medium - Redo
+2. Medium - self
+3. Easy
+4. Easy
+5. Easy - Looked at notes. Class variables
+6. Easy - Redo
+7. Easy
+8. Medium - Redo
+9. Easy - Confusion about 'private' methods
+10. Easy - Redo / Protected methods
+
+### OO Basics: Classes and Objects 1
+
+1. Easy
+2. Easy
+3. Easy
+4. Easy
+5. Easy - Redo
+6. Easy
+7. Easy
+8. Easy
+9. Easy
+10. Easy
